4a064b709feeeb529186a3297721af7725e342d6
daniel.vicenzo <daniel.vicenzo@hypeone.com>
2025-05-01 14:44:09 -0300

    Primeiro Commit, criando as entidades e script do flyway para criar as entidades no banco

---
+++ src/main/java/com/client/ws/rasmooplus/model/User.java
@@ -0,0 +1,122 @@
+package com.client.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "users_id")
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String cpf;
+
+    @Column(name = "dt_subscription")
+    private LocalDate dtSubscription = LocalDate.now();
+
+    @Column(name = "dt_expiration")
+    private LocalDate dtExpiration;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_type_id")
+    private UserType userType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subscription_type_id")
+    private SubcriptionType subcriptionType;
+
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String cpf, LocalDate dtSubscription, LocalDate dtExpiration, UserType userType, SubcriptionType subcriptionType) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.cpf = cpf;
+        this.dtSubscription = dtSubscription;
+        this.dtExpiration = dtExpiration;
+        this.userType = userType;
+        this.subcriptionType = subcriptionType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDtSubscription() {
+        return dtSubscription;
+    }
+
+    public void setDtSubscription(LocalDate dtSubscription) {
+        this.dtSubscription = dtSubscription;
+    }
+
+    public LocalDate getDtExpiration() {
+        return dtExpiration;
+    }
+
+    public void setDtExpiration(LocalDate dtExpiration) {
+        this.dtExpiration = dtExpiration;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public SubcriptionType getSubcriptionType() {
+        return subcriptionType;
+    }
+
+    public void setSubcriptionType(SubcriptionType subcriptionType) {
+        this.subcriptionType = subcriptionType;
+    }
+}
