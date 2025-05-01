ca5f034297a73256984b023561a5b9527aa4117f
daniel.vicenzo <daniel.vicenzo@hypeone.com>
2025-05-01 14:48:32 -0300

    Adicionando o Lombok na aplicação

--- src/main/java/com/client/ws/rasmooplus/model/SubcriptionType.java
+++ src/main/java/com/client/ws/rasmooplus/model/SubcriptionType.java
@@ -1,12 +1,21 @@
 package com.client.ws.rasmooplus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "subscription_type")
-public class SubcriptionType {
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubcriptionType implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,54 +28,4 @@ public class SubcriptionType {
     @Column(name = "product_key")
     private String productKey;
 
-    public SubcriptionType() {
-    }
-
-    public SubcriptionType(Long id, String name, Long accessMonth, BigDecimal price, String productKey) {
-        this.id = id;
-        this.name = name;
-        this.accessMonth = accessMonth;
-        this.price = price;
-        this.productKey = productKey;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAccessMonth() {
-        return accessMonth;
-    }
-
-    public void setAccessMonth(Long accessMonth) {
-        this.accessMonth = accessMonth;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getProductKey() {
-        return productKey;
-    }
-
-    public void setProductKey(String productKey) {
-        this.productKey = productKey;
-    }
 }
--- src/main/java/com/client/ws/rasmooplus/model/User.java
+++ src/main/java/com/client/ws/rasmooplus/model/User.java
@@ -1,12 +1,21 @@
 package com.client.ws.rasmooplus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
-public class User {
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -32,91 +41,4 @@ public class User {
     @JoinColumn(name = "subscription_type_id")
     private SubcriptionType subcriptionType;
 
-
-    public User() {
-    }
-
-    public User(Long id, String name, String email, String phone, String cpf, LocalDate dtSubscription, LocalDate dtExpiration, UserType userType, SubcriptionType subcriptionType) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.cpf = cpf;
-        this.dtSubscription = dtSubscription;
-        this.dtExpiration = dtExpiration;
-        this.userType = userType;
-        this.subcriptionType = subcriptionType;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getDtSubscription() {
-        return dtSubscription;
-    }
-
-    public void setDtSubscription(LocalDate dtSubscription) {
-        this.dtSubscription = dtSubscription;
-    }
-
-    public LocalDate getDtExpiration() {
-        return dtExpiration;
-    }
-
-    public void setDtExpiration(LocalDate dtExpiration) {
-        this.dtExpiration = dtExpiration;
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
-
-    public SubcriptionType getSubcriptionType() {
-        return subcriptionType;
-    }
-
-    public void setSubcriptionType(SubcriptionType subcriptionType) {
-        this.subcriptionType = subcriptionType;
-    }
 }
